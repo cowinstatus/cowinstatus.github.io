02eeb45a7cda273185fac80f7693bb803e3617df
cowinstatus <mpandit@mpandit-mn2.linkedin.biz>
2021-05-05 20:19:54 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Check Status</h1>
+       <h1>Check <a href="https://www.cowin.gov.in/" title="cowin site link">Cowin</a> Schedule Availability</h1>
+       <sub>This project was initially created to get schedule availability from COWIN site as cowin site didn't contain availability which are now on top page.
+         Will be adding notification feature soon. As of now, you can check if schedule is available for particular location and go to cowin site to book
+       </sub>
        <MainClass />
       </header>
     </div>
--- src/MainClass.js
+++ src/MainClass.js
@@ -8,7 +8,6 @@ export default class MainClass extends React.Component {
           hospitals: [],
           message: ''
         };
-  
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
     }
@@ -18,28 +17,50 @@ export default class MainClass extends React.Component {
     }
     processData(data) {
         // checks if any of the age is less than 45+
-        console.log('logging data & centers', data, data.centers);
         let centres = data.centers;
         let hospitals = [];
         let availablity = 'None';
-        centres.forEach(centre => {
-            centre.sessions.forEach(session=> {
-                if (session.min_age_limit < 45) {
-                    availablity = 'Available';
-                }
-            });
-            let hospitalObj = {
-                district: centre.district_name,
-                name: centre.name,
-                availablity
-            };
-            hospitals.push(hospitalObj);
-        });
-        this.setState({hospitals: hospitals});
-        this.setState({message: availablity});
+        let isAvailable = 'None';
+        if (centres && centres.length > 0) {
+          centres.forEach(centre => {
+              centre.sessions.forEach(session=> {
+                  if (session.min_age_limit < 45) {
+                      availablity = 'Available';
+                      if (session.available_capacity == 0) {
+                        availablity = 'Booked';
+                      } else {
+                        isAvailable = 'Available';
+                      }
+                  }
+              });
+              let hospitalObj = {
+                  district: centre.district_name,
+                  name: centre.name,
+                  availablity
+              };
+              hospitals.push(hospitalObj);
+          });
+          // sort hospitals by availability
+          hospitals.sort((a,b)=> {
+            if (a.availablity === "Available" && b.availablity === "None") {
+              return -1;
+            } else if (a.availablity === "None" && b.availablity === "Available") {
+              return 1;
+            }
+            return 0;
+          });
+          debugger;
+          hospitals = hospitals.filter((x, i) => i < 10);
+          this.setState({hospitals: hospitals});
+          
+      } else {
+        isAvailable = "Possible invalid pin code";
+      }
+      this.setState({message: isAvailable});
     }
     handleSubmit(event) {
       console.log('pin code request sent:', this.state.pincode);
+      this.setState({hospitals: {}});
       fetch(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${this.state.pincode}&date=01-05-2021`)
       .then(response => response.json())
       .then(data => {
@@ -51,30 +72,49 @@ export default class MainClass extends React.Component {
   
     render() {
       return (
+        <section class="body">
         <form onSubmit={this.handleSubmit}>
-          <label htmlFor="pincode">Enter pincode:
-            
-            <input type="text" id="pincode" value={this.state.pincode} onChange={this.handleChange} />
+          <label htmlFor="pincode" className="pincode-label">Enter pincode:
           </label>
-          <input type="submit" value="Check" />
+            <input type="text" id="pincode" className="pincode-text" value={this.state.pincode} onChange={this.handleChange} />
           
-            {this.state.hospitals.map(hospital=> (
-                <ul>
-                <li>
-                    {hospital.district}
-                </li>
-                <li>
-                    {hospital.name}
-                </li>
-                <li>
-                    {hospital.availablity}
-                </li>
-                </ul>
-            ))}
-            
-
-          <div>availablity: {this.state.message}</div>
+          <input type="submit" className="btn-availability" value="Check Availability" />
         </form>
+        {
+        !(this.state.message ==='' || this.state.message ==='Available') &&
+          <div className="availability-msg">Available at any place: {this.state.message}
+          </div>
+        }
+         {this.state.message === 'Available' && 
+            <span>
+              <a href="https://www.cowin.gov.in/" title="book on cowin"> Book on CoWin site</a>
+            </span>
+          }
+        {this.state.hospitals.length > 0 && 
+          <table>
+            <th>Place</th>
+            <th>Hospital</th>
+            <th>Availability</th>
+            {this.state.hospitals.map(hospital=> (
+                    <tr>
+                    <td>
+                        {hospital.district}
+                    </td>
+                    <td>
+                        {hospital.name}
+                    </td>
+                    <td>
+                        {hospital.availablity}
+                    </td>
+                    </tr>
+            ))}
+        </table>
+        }
+        <footer>
+          <a href="https://trello.com/b/G0AAwP7b/status-board-of-items">Features board</a>
+          <a href="https://github.com/cowinstatus/cowinstatus.github.io">Github</a>
+        </footer> 
+        </section>
         
       )
     }
